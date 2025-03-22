00f26b95434c856de640058e99ead3b602b8590e
KDLGates <kdlgates@kdlgates.com>
2025-03-22 19:41:55 -0400

    Fix routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,11 +18,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Serve all client routes through index
+// Serve all client routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Frontend server running on port ${PORT}`);
+});
+
+// Serve statics from public
+app.use(express.static(path.join(__dirname, '../public')));
+
+// For any route that doesn't match a static file, serve the index
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Add a specific health check endpoint for AWS
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
 });
