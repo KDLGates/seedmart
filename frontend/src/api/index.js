c963e4c200c5e22eaec109637e4c1bb988d37342
KDLGates <kdlgates@kdlgates.com>
2025-03-22 19:23:05 -0400

    Add express routes for AWS config

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,6 +2,17 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
+
+// Core application routes
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
+// Functional CRUD routes
 export const getItems = async () => {
     const response = await axios.get(`${API_URL}/items`);
     return response.data;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,20 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Logging
+app.use((req, res, next) => {
+  console.log(`Request URL: ${req.url}`);
+  console.log(`Current directory: ${__dirname}`);
+  next();
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Serve the main page
-app.get('/', (req, res) => {
+// Serve all client routes through index
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
